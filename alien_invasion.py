516cd0770ad05cf467aaf48eff1d9fdec6cf738d
Rohan Parkes <rparkes@email.com>
2025-12-02 07:44:36 +1100

    Added bullets functionality.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@
 """
 
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -16,10 +17,13 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     ship = Ship(ai_settings, screen)
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
-        gf.update_screen(ai_settings, screen, ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
 run_game()
