d0fb53bde54764a12e1eb728eda225205946c356
Rohan Parkes <rparkes@email.com>
2025-12-02 08:16:24 +1100

    Added alien code.

--- game_functions.py
+++ game_functions.py
@@ -48,10 +48,11 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """Update images on the screen and flip to the new screen."""
     screen.fill(ai_settings.bg_color)
     ship.blitme()
+    alien.blitme()
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     pygame.display.flip()
